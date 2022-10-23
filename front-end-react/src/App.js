c4547ffa4dfd08f75c43a9d7fb8b7e6a023d81b5
Amanda Sharmaine Remo <asremo@Amandas-MBP.lan>
2022-10-23 11:56:18 -0400

    added components

--- front-end-react/src/App.js
+++ front-end-react/src/App.js
@@ -1,8 +1,20 @@
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+
 import './App.css';
 
 function App() {
+
   return (
-    <h1>Hello World</h1>
+    <div className='App'>
+      <Router>
+        <header>
+          <h1 className="title">Movie List!</h1>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
