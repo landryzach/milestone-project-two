ad3bca4144ab564f0a9eaabe6c7f4563851e8a4e
cwitmer34 <54679947+cwitmer34@users.noreply.github.com>
2022-10-25 20:02:01 -0400

    fetching data

--- front-end-react/src/App.js
+++ front-end-react/src/App.js
@@ -1,19 +1,13 @@
-
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import MyList from "./components/MyList";
 import AddMovie from "./components/AddMovie";
 import EditMovie from "./components/EditMovie";
-
 // CSS
 import "./App.css";
 
-
-
 function App() {
-
   return (
-
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/list" element={<MyList />} />
--- front-end-react/src/components/MyList.jsx
+++ front-end-react/src/components/MyList.jsx
@@ -1,9 +1,20 @@
 import NavBar from "./NavBar";
 import ListItem from "./ListItem";
+import { useState } from "react";
 
 const MyList = () => {
+  const [movies, setMovies] = useState([]);
+
+  const fetchData = async () => {
+    const data = await fetch(`/movie`);
+    const jsonData = await data.json();
+    console.log(jsonData);
+    setMovies(jsonData);
+  };
+
   return (
     <>
+      <button onClick={fetchData}>fetch</button>
       <NavBar />
       <ListItem />
     </>
