b21a23d9965145594e86673617e6e118bfdfdccb
ayanbajo <bolatitoayanbajo@gmail.com>
2022-10-30 13:01:34 -0400

    made changes to component files

--- front-end-react/src/components/NewMovie.jsx
+++ front-end-react/src/components/NewMovie.jsx
@@ -1,18 +1,40 @@
 import NavBar from "./NavBar";
 
+import {useState} from 'react'
+
 const NewMovie = () => {
 
+    const [title, setTitle] = useState('')
+    console.log('title!!!', title)
+
+    const saveMovie = async ()=> {
+        console.log('We are about to save!!!!!')
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              title: title,
+              
+            })
+          };
+         // do our fetch stuff!!
+        const data = await fetch('/movie', requestOptions)
+         console.log('time to save movei!!', data)
+    }
+    
     return (
         
         <div className="Form">
             <h2>Add A New Movie</h2>
             {/* method is the HTTP verb and action is the destination path */}
-            <form method="POST" action="/movies">
+            {/* <form method="POST" action="/movie"> */}
+            <form >
                 <label>Movie Title: </label>
                 <input 
                     id="title"
                     name="title"
                     required
+                    onChange={(e)=> {setTitle(e.target.value)}}
                 />
                 <label>Image URL: </label>
                 <input 
@@ -46,8 +68,9 @@ const NewMovie = () => {
                     required
                 />
                 <input 
+                onClick={saveMovie}
                     className="btn"
-                    type="submit"
+                   // type="submit"
                     value="Add Movie"
                 />
             </form>
