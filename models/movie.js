487a017d5a4d123f18aa35fe1dd60e37d62d4cb2
ayanbajo <bolatitoayanbajo@gmail.com>
2022-10-31 19:03:45 -0400

    made changes to model file

--- models/movie.js
+++ models/movie.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose
 
 const movieSchema = new Schema ({
     title: { type: String, required: true },
-    image: { type: String, default: 'http://placekitten.com/350/350' },
+    image: { type: String, default: 'http://placekitten.com/350/500' },
     date_released: { type: Number },
     duration: { type: Number },
     genre: { type: String },
