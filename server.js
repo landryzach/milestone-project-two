47edc6b468743e70ac22e0809ba7d2d52b0901c6
Zachary Landry <landryzachary4@gmail.com>
2023-02-07 23:09:44 -0500

    updated server

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
 const movieController = require('./controller/movie_controller.js')
   app.use('/movie', movieController);
 
-// app.use(express.static(path.join(__dirname, 'front-end-react', 'build')))
+app.use(express.static(path.join(__dirname, 'front-end-react', 'build')))
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
   () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
