efdcdf0681bbf5494ea4619714a30e0f65ed4307
Zachary Landry <landryzachary4@gmail.com>
2023-02-10 19:51:36 -0500

    updated app.get on server

--- server.js
+++ server.js
@@ -7,18 +7,18 @@ const path = require('path')
 
 app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
-
 const movieController = require('./controller/movie_controller.js')
   app.use('/movie', movieController);
-
-app.use(express.static(path.join(__dirname, 'front-end-react', 'build')))
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
   () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
 )
+
+app.use(express.static(path.join(__dirname, 'front-end-react', 'build')))
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'front-end-react', 'build'));
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`App is listening on port`, process.env.PORT)
